da2a497a1b658bca0fe73636e828cde3d46bb43a
Priscila c Iriart <pris.iriart@gmail.com>
2024-08-27 15:25:30 -0300

    sacando js creados de mas no se porque

--- js/links.js
+++
@@ -1,6 +0,0 @@
-// Agregar links al nav
-export const links = [
-  { texto: 'Inicio', href: 'index.html' },
-  { texto: 'conversion', href: 'html/conversion.html' },
-  { texto: 'comparador', href: '#' },
-];
--- js/opciones.js
+++
@@ -1,5 +0,0 @@
-export const opciones = [
-    { texto: 'Inicio', href: '' },
-    { texto: 'Conversion', href: './html/conversion.html' },
-    { texto: 'Comparador', href: '' },
-];
